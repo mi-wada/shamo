e40315f33c9a4b29994bfbd60c4fbeb37ac3a0d0
mi-wada <mtak.wd@gmail.com>
2023-09-15 17:42:27 +0900

    Fix tsc

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,36 +1,3 @@
-"use client";
-
-import { Button } from "@/components/common/Button";
-import { Loading } from "@/components/common/Loading";
-
 export default function Home() {
-  return (
-    <main>
-      <div>
-        <Button
-          loading={true}
-          color="error"
-          onClick={() => {
-            alert("hello");
-          }}
-        >
-          Register
-        </Button>
-      </div>
-      <div>
-        <Button
-          loading={false}
-          color="primary"
-          onClick={() => {
-            alert("hello");
-          }}
-        >
-          Register
-        </Button>
-      </div>
-      <div>
-        <Loading size="small" />
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
