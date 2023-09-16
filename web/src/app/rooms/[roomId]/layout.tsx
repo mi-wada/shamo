d15123590b168494bd2ccecd691d9f67ae68b8f8
mi-wada <mtak.wd@gmail.com>
2023-09-16 09:21:50 +0900

    Add layout for /rooms/:roomId

---
+++ web/src/app/rooms/[roomId]/layout.tsx
@@ -0,0 +1,15 @@
+import { TogglePageButton } from "@/features/rooms";
+import { Box } from "@mui/material";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <Box sx={{ display: "flex", justifyContent: "center", margin: "4px" }}>
+        <TogglePageButton />
+      </Box>
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
