78abf625e9f6d312ef8e630a13b783e93cc62e29
mi-wada <mtak.wd@gmail.com>
2023-09-16 18:24:01 +0900

    Align TogglePageButton

--- web/src/app/rooms/[roomId]/layout.tsx
+++ web/src/app/rooms/[roomId]/layout.tsx
@@ -4,7 +4,7 @@ import { Box } from "@mui/material";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      <Box sx={{ display: "flex", justifyContent: "center", margin: "4px" }}>
+      <Box sx={{ display: "flex", justifyContent: "center", margin: "8px" }}>
         <TogglePageButton />
       </Box>
       {children}
