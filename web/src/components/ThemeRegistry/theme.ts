cf82903839b8e07c5c856ce2bd47ed33b2493c00
mi-wada <mtak.wd@gmail.com>
2023-09-16 18:23:32 +0900

    Define theme for typography

--- web/src/components/ThemeRegistry/theme.ts
+++ web/src/components/ThemeRegistry/theme.ts
@@ -5,4 +5,18 @@ export const theme = (darkmode: boolean) =>
     palette: {
       mode: darkmode ? "dark" : "light",
     },
+    typography: {
+      h1: {
+        fontSize: "2rem",
+        fontWeight: 500,
+      },
+      h2: {
+        fontSize: "1.5rem",
+        fontWeight: 500,
+      },
+      h3: {
+        fontSize: "1.17rem",
+        fontWeight: 500,
+      },
+    },
   });
