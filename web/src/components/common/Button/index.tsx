70d1e3a7a59df3f458994b1a0d73bd88a38cd430
mi-wada <mtak.wd@gmail.com>
2023-09-16 18:28:09 +0900

    Add arg to Button

--- web/src/components/common/Button/index.tsx
+++ web/src/components/common/Button/index.tsx
@@ -1,16 +1,22 @@
 import { Button as MuiButton, type ButtonProps as MuiButtonProps } from "@mui/material";
+import { Loading } from "../Loading";
 
 export type ButtonProps = {
   /**
    * @default false
    */
   loading?: boolean;
+  /**
+   * @default false
+   */
+  disabled?: boolean;
+  color?: MuiButtonProps["color"];
   onClick: () => void;
   children: React.ReactNode;
 };
 
-export const Button = ({ loading, children, ...props }: ButtonProps) => (
-  <MuiButton disabled={loading} {...props}>
-    {loading ? "Loading..." : children}
+export const Button = ({ loading, disabled, children, ...props }: ButtonProps) => (
+  <MuiButton variant="outlined" disabled={loading || disabled} {...props}>
+    {loading ? <Loading size="small" /> : children}
   </MuiButton>
 );
