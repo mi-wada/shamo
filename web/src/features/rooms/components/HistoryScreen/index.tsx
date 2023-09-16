40a4b78d2eb166ef92633c974548b6eee03337b2
mi-wada <mtak.wd@gmail.com>
2023-09-16 18:30:48 +0900

    Add HistoryScreen

---
+++ web/src/features/rooms/components/HistoryScreen/index.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import { PaymentHistory } from "../PaymentHistory";
+
+type HistoryScreenProps = {
+  roomId: string;
+};
+
+export const HistoryScreen = ({ roomId }: HistoryScreenProps) => {
+  return (
+    <Box sx={{ margin: "8px" }}>
+      <PaymentHistory roomId={roomId} />
+    </Box>
+  );
+};
--- web/src/features/rooms/index.ts
+++ web/src/features/rooms/index.ts
@@ -1,1 +1,2 @@
 export * from "./components/TogglePageButton";
+export * from "./components/HistoryScreen";
