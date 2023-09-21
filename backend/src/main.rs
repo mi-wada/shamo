9f73b459e16ad83539d22af3829bb9f06b001815
mi-wada <mtak.wd@gmail.com>
2023-09-22 07:37:45 +0900

    Add deploy workflow for backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,41 @@
-use std::error::Error;
+use axum::{http::StatusCode, Router};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let env_file_name = format!(".env.{}", std::env::var("ENV")?);
-    dotenvy::from_filename(env_file_name)?;
+#[tokio::main]
+async fn main() {
+    let env_file_name = format!(".env.{}", std::env::var("ENV").expect("ENV not set"));
+    dotenvy::from_filename(env_file_name).expect(&format!(
+        "Failed to load .env.{}",
+        std::env::var("ENV").unwrap()
+    ));
 
-    println!("{}", dotenvy::var("ENV")?);
+    println!("{}", dotenvy::var("ENV").unwrap());
 
-    Ok(())
+    let app = Router::new().route("/health", axum::routing::get(|| async { StatusCode::OK }));
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
