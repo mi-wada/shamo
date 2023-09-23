1618b26c4d57351cf04b66f4805ee623033f7c00
mi-wada <mtak.wd@gmail.com>
2023-09-23 20:11:41 +0900

    Remove unnecessary }

--- web/src/features/rooms/hooks/useDeletePayment.ts
+++ web/src/features/rooms/hooks/useDeletePayment.ts
@@ -9,7 +9,7 @@ export const useDeletePayment = ({ onSuccess }: { onSuccess: () => Promise<void>
   return useMutation<any, ShamoApiErrorResponse, MutateProps>({
     method: "DELETE",
     url: (props: MutateProps) =>
-      `${process.env.NEXT_PUBLIC_SHAMO_API_BASE_URL}/rooms/${props.roomId}/payments/${props.paymentId}}`,
+      `${process.env.NEXT_PUBLIC_SHAMO_API_BASE_URL}/rooms/${props.roomId}/payments/${props.paymentId}`,
     onSuccess: onSuccess,
   });
 };
