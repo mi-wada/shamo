de386cc8aee4e4d90864ef3c358da729dd2ec2f3
mi-wada <mtak.wd@gmail.com>
2024-11-01 23:16:07 +0900

    Introduce biome

--- new_backend/src/index.ts
+++ new_backend/src/index.ts
@@ -1,9 +1,9 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/healthz", (c) => {
+  return c.text("ok");
+});
 
-export default app
+export default app;
